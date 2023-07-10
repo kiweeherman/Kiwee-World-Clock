9b94e33927963598338a89dc33003a9809c58a92
kiweeherman <137016035+kiweeherman@users.noreply.github.com>
2023-07-09 21:10:54 -0500

    Added all options, and default time

--- src/action.js
+++ src/action.js
@@ -1,50 +1,56 @@
 // This section will be soley for making the other world clocks work (i.e. New York, Tokyo, and London)
-//New York
+//New York Setup
 let newYorkDate = document.getElementById("new-york-date");
 let newYorkTime = document.getElementById("new-york-time");
-function newYorkCurrentDate() {
-  newYorkDate.innerHTML = moment()
-    .tz("America / New_York")
-    .format("MMMM Do, YYYY");
-}
-setTimeout(newYorkCurrentDate, 1);
-setInterval(newYorkCurrentDate, 60000);
 
-// Time in New York
-function newYorkCurrentTime() {
-  newYorkTime.innerHTML = moment().tz("America/New York").format("LTS");
-}
-setTimeout(newYorkCurrentTime, 1);
-setInterval(newYorkCurrentTime, 1000);
-
-// Tokyo
+// Tokyo Setup
 let tokyoDate = document.getElementById("tokyo-date");
 let tokyoTime = document.getElementById("tokyo-time");
-function tokyoCurrentDate() {
-  tokyoDate.innerHTML = moment().tz("Asia / Tokyo").format("MMMM Do, YYYY");
-}
-setTimeout(tokyoCurrentDate, 1);
-setInterval(tokyoCurrentDate, 60000);
 
-// Time in Tokyo
-function tokyoCurrentTime() {
-  tokyoTime.innerHTML = moment().tz("Asia/Tokyo").format("LTS");
-}
-setTimeout(tokyoCurrentTime, 1);
-setInterval(tokyoCurrentTime, 1000);
-
-//London
+//London Setup
 let londonDate = document.getElementById("london-date");
 let londonTime = document.getElementById("london-time");
-function londonCurrentDate() {
-  londonDate.innerHTML = moment().tz("Europe / London").format("MMMM Do, YYYY");
-}
-setTimeout(londonCurrentDate, 1);
-setInterval(londonCurrentDate, 60000);
 
-// London Time
-function londonCurrentTime() {
-  londonTime.innerHTML = moment().tz("Europe/London").format("LTS");
+// Lets deafult the first city to Hanover, PA for funzies
+let currentCityName = document.getElementById("current-city-name");
+let currentCityDate = document.getElementById("current-city-date");
+let currentCityTime = document.getElementById("current-city-time");
+
+// Date Auto Updates
+function currentDate() {
+  londonDate.innerHTML = moment().tz("Europe / London").format("MMMM Do, YYYY");
+  newYorkDate.innerHTML = moment().tz("EST5EDT").format("MMMM Do, YYYY");
+  tokyoDate.innerHTML = moment().tz("Asia / Tokyo").format("MMMM Do, YYYY");
+  currentCityDate.innerHTML = moment().tz("EST5EDT").format("MMMM Do, YYYY");
 }
-setTimeout(londonCurrentTime, 1);
-setInterval(londonCurrentTime, 1000);
+setTimeout(currentDate, 1);
+setInterval(currentDate, 60000);
+
+// Time Auto updates
+function currentTime() {
+  newYorkTime.innerHTML = moment().tz("EST5EDT").format("LTS");
+  tokyoTime.innerHTML = moment().tz("Asia/Tokyo").format("LTS");
+  londonTime.innerHTML = moment().tz("Europe/London").format("LTS");
+  currentCityTime.innerHTML = moment().tz("EST5EDT").format("LTS");
+}
+setTimeout(currentTime, 1);
+setInterval(currentTime, 1000);
+
+// Now for the selected timezone
+
+selector = document.getElementById("city-select");
+
+function changeFirst(data) {
+  if (data.target.value === "Blank") {
+    alert("Please select a city!");
+  } else if (data.target.value === "CurrentLocation") {
+  } else {
+    currentCityDate.innerHTML = moment()
+      .tz(data.target.value)
+      .format("MMMM Do, YYYY");
+    currentCityTime.innerHTML = moment().tz(data.target.value).format("LTS");
+    currentCityName.innerHTML = data.target.value;
+  }
+}
+
+selector.addEventListener("change", changeFirst);
