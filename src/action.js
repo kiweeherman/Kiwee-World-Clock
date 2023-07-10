ad656f784f26a9f95c6e93dad320839de2a92271
kiweeherman <137016035+kiweeherman@users.noreply.github.com>
2023-07-09 21:40:20 -0500

    Minor big fixes

--- src/action.js
+++ src/action.js
@@ -16,14 +16,18 @@ let currentCityName = document.getElementById("current-city-name");
 let currentCityDate = document.getElementById("current-city-date");
 let currentCityTime = document.getElementById("current-city-time");
 
+let timeZoneGuess = "EST5EDT";
+
 // Date Auto Updates
 function currentDate() {
   londonDate.innerHTML = moment().tz("Europe / London").format("MMMM Do, YYYY");
   newYorkDate.innerHTML = moment().tz("EST5EDT").format("MMMM Do, YYYY");
   tokyoDate.innerHTML = moment().tz("Asia / Tokyo").format("MMMM Do, YYYY");
-  currentCityDate.innerHTML = moment().tz("EST5EDT").format("MMMM Do, YYYY");
+  currentCityDate.innerHTML = moment()
+    .tz(timeZoneGuess)
+    .format("MMMM Do, YYYY");
 }
-setTimeout(currentDate, 1);
+setTimeout(currentDate, 2);
 setInterval(currentDate, 60000);
 
 // Time Auto updates
@@ -31,9 +35,9 @@ function currentTime() {
   newYorkTime.innerHTML = moment().tz("EST5EDT").format("LTS");
   tokyoTime.innerHTML = moment().tz("Asia/Tokyo").format("LTS");
   londonTime.innerHTML = moment().tz("Europe/London").format("LTS");
-  currentCityTime.innerHTML = moment().tz("EST5EDT").format("LTS");
+  currentCityTime.innerHTML = moment().tz(timeZoneGuess).format("LTS");
 }
-setTimeout(currentTime, 1);
+setTimeout(currentTime, 2);
 setInterval(currentTime, 1000);
 
 // Now for the selected timezone
@@ -41,15 +45,28 @@ setInterval(currentTime, 1000);
 selector = document.getElementById("city-select");
 
 function changeFirst(data) {
+  let newName = data.target.value;
+  if (data.target.value.includes("/") === true) {
+    newName = data.target.value.replace("_", " ").split("/")[1];
+  } else {
+    newName = data.target.value;
+  }
   if (data.target.value === "Blank") {
     alert("Please select a city!");
-  } else if (data.target.value === "CurrentLocation") {
-  } else {
+  } else if (data.target.value === "Your Location") {
+    timeZoneGuess = moment.tz.guess();
     currentCityDate.innerHTML = moment()
-      .tz(data.target.value)
+      .tz(timeZoneGuess)
       .format("MMMM Do, YYYY");
-    currentCityTime.innerHTML = moment().tz(data.target.value).format("LTS");
-    currentCityName.innerHTML = data.target.value;
+    currentCityTime.innerHTML = moment().tz(timeZoneGuess).format("LTS");
+    currentCityName.innerHTML = `${newName}<br>(${timeZoneGuess})`;
+  } else {
+    timeZoneGuess = data.target.value;
+    currentCityDate.innerHTML = moment()
+      .tz(timeZoneGuess)
+      .format("MMMM Do, YYYY");
+    currentCityTime.innerHTML = moment().tz(timeZoneGuess).format("LTS");
+    currentCityName.innerHTML = newName;
   }
 }
 
